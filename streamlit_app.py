31738ad3ca42641ea2cf3536f1a43fd5e159767e
jiarui9 <89852395+jiarui9@users.noreply.github.com>
2024-04-19 17:16:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import pandas as pd
-import load_model
+from tensorflow.keras.models import load_model
 import numpy as np
 
-# Load the model
+# Load your trained model
 model = load_model('MLP_model.h5')
 
 # Define the structure of your web app
 st.title('Domain Name Price Prediction')
-
-# File uploader allows user to add their own CSV
-uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
