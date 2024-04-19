1fe8d5dd7734274849399d8114c6d4268f0cb67d
jiarui9 <89852395+jiarui9@users.noreply.github.com>
2024-04-19 17:07:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-# from tensorflow.keras.models import load_model
+import load_model
 import numpy as np
 
 # Load the model
-# model = load_model('MLP_model.h5')
+model = load_model('MLP_model.h5')
 
 # Define the structure of your web app
 st.title('Domain Name Price Prediction')
