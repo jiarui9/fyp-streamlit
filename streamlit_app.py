a0068e45326c2511fbcd04fc1ba46c8e0083e101
jiarui9 <89852395+jiarui9@users.noreply.github.com>
2024-04-19 16:50:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,14 @@ def main():
     Domain_Length_Score = st.number_input("Domain Length Score", min_value=0, max_value=100, value=50)
 
     # When the 'Predict' button is clicked, make the prediction and display it
-    if st.button("Predict"):
-        # Create the feature array
-        features = np.array([[SEO_Score, Domain_Age, TLD_Score, Search_Occurrences,
-                              Word_Composition_Score, Domain_Length_Score]])
-        # Predict the price
-        # price = model.predict(features)
-        # Display the prediction
-        st.success(f"The predicted price of the domain is: ${price[0][0]:.2f}")
+    # if st.button("Predict"):
+    #     # Create the feature array
+    #     features = np.array([[SEO_Score, Domain_Age, TLD_Score, Search_Occurrences,
+    #                           Word_Composition_Score, Domain_Length_Score]])
+    #     # Predict the price
+    #     price = model.predict(features)
+    #     # Display the prediction
+    #     st.success(f"The predicted price of the domain is: ${price[0][0]:.2f}")
 
 if __name__ == "__main__":
     main()
